f90e139adae538c364a9c4e536cc8d0d14286f34
kubra <kubra.roshi@gmail.com>
2023-09-27 15:03:44 +0530

    added borders and margins

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+function Username() {
+  return (
+    <div className="text-sm font-semibold hidden">Kubra</div>
+  )
+}
+
+export default Username
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
+import Username from "../features/user/Username"
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-        <Link to="/">Fast React pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200">
+        <Link className="tracking-widest" to="/">Fast React pizza Co.</Link>
         <SearchOrder/>
+        <Username/>
     </header>
   )
 }
