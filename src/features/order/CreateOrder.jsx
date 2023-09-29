0b7a7e21363c09350b403b4b94927a57d44e0e9a
kubra <kubra.roshi@gmail.com>
2023-09-29 16:37:16 +0530

    added userslice in redux

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@
 import { Form, redirect, useActionData,  useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -32,6 +33,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username)
   const navigation = useNavigation()
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
@@ -47,7 +49,7 @@ function CreateOrder() {
 
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input w-full" type="text" name="customer" required />
+          <input className="input w-full" type="text" name="customer" defaultValue={username} required />
         </div>
 
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
